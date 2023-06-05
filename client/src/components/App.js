b3ec6f4a52c6dfe1d71d9f7b4414ca988cf54967
Amartya1110 <amartyakhan999@gmail.com>
2023-06-05 12:18:56 +0530

    Client-side-routing configured

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,26 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Home Page</div>
+    },
+    {
+        path: "/register",
+        element: <div>Register Page</div>
+    }
+])
+
 const App = () => {
     return(
-        <>
+        <RouterProvider router={appRouter}>
             <h1 className="bg-orange-600" >React Frontend</h1>
-        </>
+        </RouterProvider>
     )
 }
+
+
+
+
 
 export default App
